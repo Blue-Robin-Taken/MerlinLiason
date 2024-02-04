047fe46cfb3b7e7619b062d24c1ff926cb716a7d
Blue Robin <julianjohnt@gmail.com>
2024-02-03 23:44:51 -0600

    testing out the update file

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 git init
 git remote add origin git@github.com:Blue-Robin-Taken/MerlinLiason.git
 git reset --hard HEAD
+git pull git@github.com:Blue-Robin-Taken/MerlinLiason.git
 
 chmod u+x setPermissions.sh
 chmod u+x update.sh

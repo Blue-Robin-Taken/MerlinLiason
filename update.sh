196367e0dc4a4df010d1ed83791e6fee3ddd1121
Blue Robin <julianjohnt@gmail.com>
2024-02-03 23:39:03 -0600

    sh file set permission automatically

--- update.sh
+++ update.sh
@@ -2,5 +2,8 @@ git init
 git remote add origin git@github.com:Blue-Robin-Taken/MerlinLiason.git
 git reset --hard HEAD
 git pull
+
+chmod u+x setPermissions.sh
+chmod u+x update.sh
 ./setPermissions.sh
 echo "updated"

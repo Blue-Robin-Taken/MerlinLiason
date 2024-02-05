29e88f194577b4a8195381511be87ec045cccaf9
Stylo <136652260+stylo-codes-stuff@users.noreply.github.com>
2024-02-05 13:03:45 -0600

    Update config.sh

--- config.sh
+++ config.sh
@@ -5,4 +5,8 @@ echo "if you aren't looking to make any changes or don't have a fork leave it em
 read currentFork
 currentfork="git@github.com:"+=$currentFork
 echo "your commits will now be made to $currentfork"
+echo "how much MB of memory are you willing to allocate to your server(we reccomend 300mb for relatively chatty servers)"
+read LiasonMEMORY
+echo "you have allocated $LiasonMEMORY MB of memory to merlin liason"
+export LiasonMEMORY
 export currentfork

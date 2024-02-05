8150b87b42050ea1a20dedec2175e7b5c3efb0f0
Stylo <136652260+stylo-codes-stuff@users.noreply.github.com>
2024-02-05 11:24:33 -0600

    Update update.sh
    oversight that i might write code for

--- update.sh
+++ update.sh
@@ -2,7 +2,8 @@ git init
 git remote add origin git@github.com:Blue-Robin-Taken/MerlinLiason.git
 git reset --hard HEAD
 git pull git@github.com:Blue-Robin-Taken/MerlinLiason.git
-
+#what if the person that pulls isnt you???
+#probably should add a config file or notice
 chmod u+x setPermissions.sh
 chmod u+x update.sh
 ./setPermissions.sh

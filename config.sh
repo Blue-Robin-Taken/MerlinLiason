96048626cd01e4821947b403914e7e14d3a6d562
Stylo <136652260+stylo-codes-stuff@users.noreply.github.com>
2024-02-05 12:22:57 -0600

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,6 +1,7 @@
 #scripts for setting configuration
 #idk anything about sh so im saving them to env variables
 echo "please enter the name of your current fork(ex. Blue-Robin-Taken/MerlinLiason.git)"
+echo "if you aren't looking to make any changes or don't have a fork leave it empty"
 read currentFork
 currentfork="git@github.com:"+=$currentFork
 echo "your commits will now be made to $currentfork"
